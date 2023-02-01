7bf57b37bf2136af1130a4e25c18da136d3f982c
Viktor Gievoi <gievoi.v@gmail.com>
2023-02-01 20:11:15 +0100

    Fix

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     platform: 'github',
-    logLevel: 'debug',
+    logLevel: 'info',
     onboardingConfig: {
         extends: ['config:base'],
     },
     repositories: ['vasary/tac-core'],
     includeForks: true,
-    gitAuthor: "Renovate bot <bot@renovate.io>",
+    gitAuthor: "Renovate bot <bot@renovate.tac.com>",
     username: "renovate",
     onboarding: false,
     printConfig: true,
