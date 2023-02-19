b04125b7004c496e5dddaa862ad2efe6527d8108
Viktor Vasary <gievoi.v@gmail.com>
2023-02-19 22:55:27 +0100

    Product remove feature fix

--- tests/Presentation/API/Product/ProductCouldBeRemovedTest.php
+++ tests/Presentation/API/Product/ProductCouldBeRemovedTest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Tests\Presentation\API\Product;
 
@@ -9,6 +9,7 @@ use App\Infrastructure\Json\Json;
 use App\Infrastructure\Test\AbstractWebTestCase;
 use App\Infrastructure\Test\Atom\AssertAttributeTrait;
 use App\Infrastructure\Test\Atom\AssertEventTrait;
+use App\Infrastructure\Test\Context\Model\CategoryContext;
 use App\Infrastructure\Test\Context\Model\ProductContext;
 use App\Infrastructure\Test\Context\Model\UserContext;
 
@@ -16,18 +17,23 @@ final class ProductCouldBeRemovedTest extends AbstractWebTestCase
 {
     use AssertAttributeTrait, AssertEventTrait;
 
-    protected static array $ids = [
-        '4170f8aa-b4ea-4f6d-b99d-57338808f299',
-    ];
+    private const PRODUCT_REMOVED_EVENT = '{"product":{"id":"1884fcbf-6ade-49a4-b91a-505290ec1e77","name":"name","description":"description","creator":"mock|10101011","attributes":[],"category":"6b58caa4-0571-44db-988a-8a75f86b2520","units":[],"createdAt":"2022-01-01T00:00:00+00:00","updatedAt":"2022-01-01T00:00:00+00:00","deletedAt":null}}';
 
     public function testShouldSuccessfullyRemoveProduct(): void
     {
         $this->freezeTime('2022-09-20 23:00:00');
 
+        $this->expectEvents([
+            [
+                'product.removed', self::PRODUCT_REMOVED_EVENT
+            ]
+        ]);
+
         $user = UserContext::create()();
         $product = ProductContext::create()();
+        $category = CategoryContext::create()();
 
-        $this->load($user, $product);
+        $this->load($user, $category, $product);
         $this->withUser($user);
 
         $response = $this->sendRequest('DELETE', '/api/products/' . $product->getId());
