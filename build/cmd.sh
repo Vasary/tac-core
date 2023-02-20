a457a377fbb329acbd61d3be080696550e20f494
Viktor Vasary <gievoi.v@gmail.com>
2023-02-20 18:54:35 +0100

    Fix cmd

--- build/cmd.sh
+++ build/cmd.sh
@@ -6,19 +6,19 @@ stat(){
     local message="$1"
     local status="$2"
     local columns=$((COLUMNS-8))
-    printf "%-*s [%s] \n" "$columns" "$message" "$status"
+    printf "[ %s ] %-*s %s\n" "$status" "$columns"  "$message"
 }
 
-rm mkdir -p /app/resource && stat "Resource directory created" " OK " || stat "Failed to create resource directory" " ERROR "
-touch /app/resource/events.log && stat "Events.log created" " OK " || stat "Failed to create events.log" " ERROR "
-chmod 0760 -R /app/resource && stat "Fix resource directory chown" " OK " || stat "Failed to change resource directory permissions" " ERROR "
+mkdir -p /app/resource > /dev/null && stat "Resource directory created" "OK" || stat "Failed to create resource directory" "FAIL"
+touch /app/resource/events.log > /dev/null && stat "Events.log created" "OK" || stat "Failed to create events.log" "FAIL"
+chmod 0760 -R /app/resource > /dev/null && stat "Fix resource directory chown" "OK" || stat "Failed to change resource directory permissions" "FAIL"
 
-rm mkdir -p /app/var && stat "Var directory created" " OK " || stat "Failed to create var directory" " ERROR "
-chmod 0777 -R /app/var && stat "Fix var directory chown" " OK " || stat "Failed to change var directory permissions" " ERROR "
+mkdir -p /app/var > /dev/null && stat "Var directory created" "OK" || stat "Failed to create var directory" "FAIL"
+chmod 0777 -R /app/var > /dev/null && stat "Fix var directory chown" "OK" || stat "Failed to change var directory permissions" "FAIL"
 
-chown www-data:www-data -R /app && stat "Fix application directory permission" " OK " || stat "Failed to fix application directory permissions" " ERROR "
+chown www-data:www-data -R /app > /dev/null && stat "Fix application directory permission" "OK" || stat "Failed to fix application directory permissions" "FAIL"
 
-/app/bin/console cache:warmup && stat "Warmup cache" " OK " || stat "Failed to warmup cache" " ERROR "
-/app/bin/console do:mi:mi --no-interaction && stat "Run migrations" " OK " || stat "Failed to run migrations" " ERROR "
+/app/bin/console cache:warmup > /dev/null && stat "Warmup cache" "OK" || stat "Failed to warmup cache" "FAIL"
+/app/bin/console do:mi:mi --no-interaction > /dev/null && stat "Run migrations" "OK" || stat "Failed to run migrations" "FAIL"
 
 sh /usr/local/bin/docker-entrypoint.sh
