e44d91c3e0ede3107bdcdd8c3907e67e84021181
Viktor Vasary <gievoi.v@gmail.com>
2023-02-20 18:07:37 +0100

    Fix migration

--- src/Infrastructure/Persistence/Doctrine/Migration/Version20230220165038.php
+++ src/Infrastructure/Persistence/Doctrine/Migration/Version20230220165038.php
@@ -1,11 +1,12 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Infrastructure\Persistence\Doctrine\Migration;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+
 final class Version20230220165038 extends AbstractMigration
 {
     public function getDescription(): string
@@ -46,7 +47,9 @@ final class Version20230220165038 extends AbstractMigration
         , updated_at DATETIME NOT NULL --(DC2Type:datetimetz_immutable)
         , deleted_at DATETIME DEFAULT NULL --(DC2Type:datetimetz_immutable)
         , PRIMARY KEY(id), CONSTRAINT FK_E6D185B861220EA6 FOREIGN KEY (creator_id) REFERENCES users (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_E6D185B8B6E62EFA FOREIGN KEY (attribute_id) REFERENCES attributes (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_E6D185B84584665A FOREIGN KEY (product_id) REFERENCES products (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_E6D185B8B6E62EFA4584665A3D8E604F ON attributes_values (attribute_id, product_id, parent)');
+        $this->addSql(
+            'CREATE UNIQUE INDEX UNIQ_E6D185B8B6E62EFA4584665A3D8E604F ON attributes_values (attribute_id, product_id, parent)'
+        );
         $this->addSql('CREATE INDEX IDX_E6D185B83D8E604F ON attributes_values (parent)');
         $this->addSql('CREATE INDEX IDX_E6D185B84584665A ON attributes_values (product_id)');
         $this->addSql('CREATE INDEX IDX_E6D185B8B6E62EFA ON attributes_values (attribute_id)');
@@ -72,7 +75,9 @@ final class Version20230220165038 extends AbstractMigration
         , parent_id BLOB NOT NULL --(DC2Type:id)
         , locale VARCHAR(255) NOT NULL COLLATE "BINARY" --(DC2Type:locale)
         , field VARCHAR(255) NOT NULL COLLATE "BINARY", value VARCHAR(255) DEFAULT NULL COLLATE "BINARY", PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_B0850B43727ACA704180C6985BF54558 ON glossary (parent_id, locale, field)');
+        $this->addSql(
+            'CREATE UNIQUE INDEX UNIQ_B0850B43727ACA704180C6985BF54558 ON glossary (parent_id, locale, field)'
+        );
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\SqlitePlatform'."
