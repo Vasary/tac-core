e03c8c5f95bc921fdb4fe3e30d559218683aaeaf
Viktor Vasary <gievoi.v@gmail.com>
2023-02-20 18:00:58 +0100

    Fix cmd.sh

--- build/cmd.sh
+++ build/cmd.sh
@@ -2,12 +2,20 @@
 
 set -e
 
+printf "Create directories:\n"
+
+mkdir -p /app/resource
+mkdir -p /app/var
+chmod 0777 -R /app/var
+chmod 0760 -R /app/resource
+chown www-data:www-data -R /app
+
 printf "Preparing cache:\n"
 
-/app/bin/console cache:warmup || exit 1
+/app/bin/console cache:warmup
 
 printf "Run migrations:\n"
 
-/app/bin/console do:mi:mi --no-interaction || exit 1
+/app/bin/console do:mi:mi --no-interaction
 
 sh /usr/local/bin/docker-entrypoint.sh
