85468d0eb223945ef7fc465e1cc08965d0f059a2
Viktor Vasary <gievoi.v@gmail.com>
2023-02-28 18:47:08 +0100

    Update deployment chart

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,24 +2,25 @@
 
 set -e
 
+/usr/local/bin/docker-php-entrypoint
+
 stat() {
   printf "[ %s ] %s\n" "$2" "$1"
 
   if [ "$1" = "Fail" ]; then
-    exit 1;
+    exit 1
   fi
 }
 
-mkdir -p /app/resource > /dev/null && stat "Resource directory created" "OK" || stat "Failed to create resource directory" "FAIL"
-mkdir -p /app/var > /dev/null && stat "Var directory created" "OK" || stat "Failed to create var directory" "FAIL"
+if [ ! -e /app/resource/events.log ]; then
+  touch /app/resource/events.log >/dev/null && stat "Events.log created" "OK" || stat "Failed to create events.log" "FAIL"
+fi
 
-chmod 0760 -R /app/resource > /dev/null && stat "Fix resource directory chown" "OK" || stat "Failed to change resource directory permissions" "FAIL"
-chmod 0777 -R /app/var > /dev/null && stat "Fix var directory chown" "OK" || stat "Failed to change var directory permissions" "FAIL"
+if [ ! -d /app/var ]; then
+  mkdir -p /app/var >/dev/null && stat "Var directory created" "OK" || stat "Failed to create var directory" "FAIL"
+  chmod 0777 -R /app/var >/dev/null && stat "Fix var directory chown" "OK" || stat "Failed to change var directory permissions" "FAIL"
+fi
 
-touch /app/resource/events.log > /dev/null && stat "Events.log created" "OK" || stat "Failed to create events.log" "FAIL"
+/app/bin/console cache:warmup >/dev/null && stat "Warmup cache" "OK" || stat "Failed to warmup cache" "FAIL"
 
-#/app/bin/console cache:warmup > /dev/null && stat "Warmup cache" "OK" || stat "Failed to warmup cache" "FAIL"
-#/app/bin/console do:mi:mi --no-interaction > /dev/null && stat "Run migrations" "OK" || stat "Failed to run migrations" "FAIL"
-
-printf "\n\nStarting PHP-FPM daemon\n\n"
-php-fpm --daemonize
+exec "$@"
